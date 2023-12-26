7a27933bf82d66caa8126659621b8e76ac854a4c
sawada <123456@abc.com>
2023-12-26 15:25:52 +0900

    init

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		
+		//ログイン画面で入力された値を取得
+		String user_id = request.getParameter("user_id");
+		String password = request.getParameter("password");
+		
+		System.out.println(user_id);
+		System.out.println(password);
+		
+		request.getRequestDispatcher("/WEB-INF/view/accept.jsp").forward(request, response);
+		
+		
+		
+	}
+
+}
